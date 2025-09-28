23fb49c4c453c4967a98bbb3015ec0149181df42
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2025-09-28 15:53:09 -0700

    cat walks

--- In Class Activities Finished/Assets/Scripts/Player.cs
+++ In Class Activities Finished/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 1.0f;
     [SerializeField] private float _turnSpeed = 1.0f;
+    [SerializeField] private Animator _animator;
+
+    private string _isWalkingName = "IsWalking";
 
     void Update()
     {
@@ -12,5 +15,14 @@ public class Player : MonoBehaviour
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
+
+        if (translation != 0.0f || rotation != 0.0f)
+        {
+            _animator.SetBool(_isWalkingName, true);
+        }
+        else
+        {
+            _animator.SetBool(_isWalkingName, false);
+        }
     }
 }
