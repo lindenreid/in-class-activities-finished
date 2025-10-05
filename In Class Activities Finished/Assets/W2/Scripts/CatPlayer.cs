5f56742c046aab9d08784a84f092088731e23923
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2025-10-05 15:20:32 -0700

    more file reorg plus basic cat movement

---
+++ In Class Activities Finished/Assets/W2/Scripts/CatPlayer.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatPlayer : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    // The speed that the player moves at
+    [SerializeField] private float _speed;
+    // The velocity of the player's jump
+    [SerializeField] private float _jump;
+    [SerializeField] private float _kick;
+
+    private bool _facingLeft;
+    private bool _isGrounded;
+
+    // ------------------------------------------------------------------------
+    // Update is called every frame
+    private void Update()
+    {
+        // Detect input to move player left/right
+        _rigidbody.linearVelocity = new Vector2(
+            Input.GetAxis("Horizontal") * _speed,
+            _rigidbody.linearVelocity.y
+        );
+
+        // Detect input to jump
+        if (Input.GetAxis("Vertical") > 0 && _isGrounded)
+        {
+            _isGrounded = false;
+
+            _rigidbody.linearVelocity = new Vector2(
+                _rigidbody.linearVelocity.x,
+                _jump
+            );
+        }
+
+        // Change character facing 
+        if (Input.GetAxis("Horizontal") < 0 && !_facingLeft)
+        {
+            _spriteRenderer.flipX = true;
+            _facingLeft = true;
+        }
+        else if (Input.GetAxis("Horizontal") > 0 && _facingLeft)
+        {
+            _spriteRenderer.flipX = false;
+            _facingLeft = false;
+        }
+
+        // Set animation state parameters
+        _animator.SetBool("walking", _rigidbody.linearVelocity.x != 0.0f);
+    }
+
+    // ------------------------------------------------------------------------
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Pretend I wrote a beautiful metaphor describing Components, GameObjects, and Sce
 - Animals: https://assetstore.unity.com/packages/3d/characters/animals/animals-free-animated-low-poly-3d-models-260727 
 - Low-poly environment: https://assetstore.unity.com/packages/3d/environments/landscapes/low-poly-simple-nature-pack-162153 
 - Cat sprites: https://toffeecraft.itch.io/cat-pixel-mega-pack
+- Interior sprites: https://rcpstd.itch.io/interior-tileset-asset-pack-16x16 
