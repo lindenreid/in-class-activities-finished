3eb42501245716f61aa9cf2b7114fd61821a7266
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2025-10-05 17:27:31 -0700

    added step flags

--- In Class Activities Finished/Assets/W2/Scripts/Ball.cs
+++ In Class Activities Finished/Assets/W2/Scripts/Ball.cs
@@ -15,6 +15,7 @@ public class Ball : MonoBehaviour
     // This code runs whenever the ball hits something.
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // STEP 1 -------------------------------------------------------------
         // Uncomment ONE of the blow lines of code to ADD ONE
         //      to the value of the variable named '_bounces'.
         // _bounces = _bounces + 2;
@@ -22,6 +23,7 @@ public class Ball : MonoBehaviour
         // _bounces --;
         _bounces++;
         // _bounces += 1
+        // STEP 1 -------------------------------------------------------------
 
         // This line sets the value of the text on screen to the word "Bounces"
         //      plus the number of times the ball has bounced.
@@ -38,38 +40,49 @@ public class Ball : MonoBehaviour
         float g = _spriteRenderer.color.g;
         float b = _spriteRenderer.color.b;
 
+        // STEP 2 -------------------------------------------------------------
         // Uncomment ONE of the below lines to ADD 0.1 to the value of 'r'.
         // r += 0.1f
         // r * 0.1;
         r = r + 0.1f;
         // r = r * 0.1f;
         // r += 0.1;
+        // STEP 2 -------------------------------------------------------------
 
         // RGB values can only go from 0.0 - 1.0.
         // So, if the value of 'r' gets to above 1.0, we want to set it back to 0.0
         //      so that we can start over and keep increasing it again.
         if (r > 1.0f)
         {
+            // STEP 3 ---------------------------------------------------------
             // Uncomment ONE of the below lines of code to set the value of 'r' to ZERO.
             // r = 0.0f
             r = 0.0f;
             // r 0.0f;
+            // STEP 3 ---------------------------------------------------------
         }
 
+        // STEP 4 -------------------------------------------------------------
         // Uncomment the below line and fix it to SUBTRACT 0.1 from the value of 'g'.
         g -= 0.1f;
         //g -= 0.1f
+        // STEP 4 -------------------------------------------------------------
 
         if (g < 0.0f)
         {
+            // STEP 5 ---------------------------------------------------------
             // Uncomment the below line and fix it to set the value of 'g' to ONE.
             g = 1.0f;
             // g = 1.0;
+            // STEP 5 ---------------------------------------------------------
         }
 
+        // STEP 6 -------------------------------------------------------------
         // Below this comment, write a line of code to MULTIPLY 'b' by 1.2.
         b *= 1.2f;
+        // STEP 6 -------------------------------------------------------------
 
+        // STEP 7 -------------------------------------------------------------
         // Below this comment, write an IF STATEMENT:
         // IF the value of 'b' is GREATER THAN **OR** EQUAL TO 1.0,
         //      set 'b' to 0.1. 
@@ -77,6 +90,7 @@ public class Ball : MonoBehaviour
         {
             b = 0.1f;
         }
+        // STEP 7 -------------------------------------------------------------
 
         // This line assigns a new color to your Ball with the new RGB values
         //      we just created.
@@ -88,6 +102,7 @@ public class Ball : MonoBehaviour
         // We'll talk more about how this line works next week also :)
         Debug.Log(newColor);
 
+        // STEP 8 -------------------------------------------------------------
         // A simple way to calculate how bright a color will be is by
         //      averaging the R, G, B values.
         // Below this comment, write 2 lines of code to ADD the values of 
@@ -95,6 +110,7 @@ public class Ball : MonoBehaviour
         // Store the result in a variable named 'brightness'.
         float brightness = r + g + b;
         brightness = brightness / 3.0f;
+        // STEP 8 -------------------------------------------------------------
 
         // Uncomment the below line to make the 'brightness' text change in your game.
         _brightnessText.text = "brightness = " + brightness;
